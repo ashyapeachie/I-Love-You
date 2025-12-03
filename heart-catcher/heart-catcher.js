08d2a699d10ff9133398d888999a5e4c28faa45e
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-12-03 14:46:21 -0500

    adding header and commenting code setup

---
+++ heart-catcher/heart-catcher.js
@@ -0,0 +1,23 @@
+/* 
+* author: Aham
+* filename: heart-catcher.js
+* description: 
+*/
+
+// const player
+// const heart
+// const scoreValue
+
+let score = 0;
+let playerX = 170;
+
+/* player movement w arrow keys */
+
+
+/* heart falling reset */
+
+
+/* game loop */
+
+
+/* not collected heart */
